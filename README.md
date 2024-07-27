eb64d3118d65e6c1d395c2b754e488f8c2a1d33c
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-07-26 18:59:34 -0700

    Add React App, Tidy Stubs

---
+++ README.md
@@ -0,0 +1,3 @@
+# AnimRat Website
+
+This repository hosts a React project as a GitHub organization page.
---
+++ site/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>AnimRat</h1>
+      <p>Welcome to the AnimRat animation and reference website!</p>
+    </div>
+  );
+}
