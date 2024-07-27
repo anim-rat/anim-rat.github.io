8d303bbd42d53ddd4fbe00f5d526b4eb09ca4833
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-07-26 22:05:26 -0700

    Update Home.tsx

--- site/src/pages/Home.tsx
+++ site/src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="container">
       <h1>Anim Rat Website</h1>
       <p>Welcome to the AnimRat animation and reference website!</p>
-      <p>This site is a work in progress.</p>
+      <p>This site is a work in progress. Hold tight!</p>
     </div>
   );
 }
