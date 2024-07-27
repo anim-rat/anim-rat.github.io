43f511ef2f61212eb08b5736db77ea40a242e72a
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-07-26 21:41:07 -0700

    Update Home.tsx

--- site/src/pages/Home.tsx
+++ site/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import './Home.css';
 export default function Home() {
   return (
     <div className="container">
-      <h1>AnimRat</h1>
+      <h1>Anim Rat</h1>
       <p>Welcome to the AnimRat animation and reference website!</p>
       <p>This site is a work in progress.</p>
     </div>
